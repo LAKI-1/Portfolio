cf49143b5951ee7ec0137b513e4cd458407e80ad
Lakindu Induwara Samarasinghe <lakinduinduwara0916@gmail.com>
2025-04-02 23:02:04 +0530

    Fix the mobile navigation issue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-
 // Preloader
-
 window.addEventListener('load', function(){
     document.querySelector('.preloader').classList.add('opacity-0');
     setTimeout(function(){
@@ -8,21 +6,23 @@ window.addEventListener('load', function(){
     }, 1000);
 });
 
-// iTyped 
-
-window.ityped.init(document.querySelector('.iTyped'), {
-    strings: ["I'm a CS Undergraduate", "I'm a web developer", "I'm a public speaker", 'I Love Machine Learning', 'I Love leadership'],
-    loop: true
-});
+// iTyped
+if(document.querySelector('.iTyped')) {
+    window.ityped.init(document.querySelector('.iTyped'), {
+        strings: ["I'm a CS Undergraduate", "I'm a web developer", "I'm a public speaker", 'I Love Machine Learning', 'I Love leadership'],
+        loop: true
+    });
+}
 
 // Portfolio Item Filter
-
 const filterContainer = document.querySelector('.portfolio-filter'),
-    filterBtns = filterContainer.children,
-    totalFilterBtn = filterBtns.length,
-    portfolioItems = document.querySelectorAll('.portfolio-item'),
-    totalPortfolioItem = portfolioItems.length;
-    
+    portfolioItems = document.querySelectorAll('.portfolio-item');
+
+if(filterContainer) {
+    const filterBtns = filterContainer.children,
+        totalFilterBtn = filterBtns.length,
+        totalPortfolioItem = portfolioItems.length;
+
     for (let i = 0; i < totalFilterBtn; i++) {
         filterBtns[i].addEventListener("click", function(){
             filterContainer.querySelector('.active').classList.remove('active');
@@ -44,147 +44,169 @@ const filterContainer = document.querySelector('.portfolio-filter'),
             }
         });
     }
+}
 
 // Portfolio Lighbox
+const lightbox = document.querySelector('.lightbox');
 
-const lightbox = document.querySelector('.lightbox'),
-    lightboxImg = lightbox.querySelector('.lightbox-img'),
-    lightboxText = lightbox.querySelector('.caption-text'),
-    lightboxClose = lightbox.querySelector('.lightbox-close'),
-    lightboxCounter = lightbox.querySelector('.caption-counter');
+if(lightbox) {
+    const lightboxImg = lightbox.querySelector('.lightbox-img'),
+        lightboxText = lightbox.querySelector('.caption-text'),
+        lightboxClose = lightbox.querySelector('.lightbox-close'),
+        lightboxCounter = lightbox.querySelector('.caption-counter');
 
-let itemIndex = 0;
+    let itemIndex = 0;
+    const totalPortfolioItem = portfolioItems.length;
 
-for (let i = 0; i < totalPortfolioItem; i++) {
-    portfolioItems[i].addEventListener('click', function(){
-        itemIndex = i;
+    for (let i = 0; i < totalPortfolioItem; i++) {
+        portfolioItems[i].addEventListener('click', function(){
+            itemIndex = i;
+            changeItem();
+            toggleLightbox();
+        });
+    }
+
+    function toggleLightbox() {
+        lightbox.classList.toggle('open');
+    }
+
+    function changeItem() {
+        let imgSrc = portfolioItems[itemIndex].querySelector('.portfolio-img img').getAttribute('src');
+        lightboxImg.src = imgSrc;
+        lightboxText.innerHTML = portfolioItems[itemIndex].querySelector('h4').innerHTML;
+        lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalPortfolioItem;
+    }
+
+    function prevItem() {
+        if (itemIndex === 0) {
+            itemIndex = totalPortfolioItem - 1;
+        } else {
+            itemIndex--;
+        }
         changeItem();
-        toggleLightbox();
+    }
+
+    function nextItem() {
+        if (itemIndex === totalPortfolioItem - 1) {
+            itemIndex = 0;
+        } else {
+            itemIndex++;
+        }
+        changeItem();
+    }
+
+    // close lightbox
+    lightbox.addEventListener('click', function(event){
+        if(event.target === lightboxClose || event.target === lightbox){
+            toggleLightbox();
+        }
     });
 }
-
-function toggleLightbox() {
-    lightbox.classList.toggle('open');
-}
-
-function changeItem() {
-    let imgSrc = portfolioItems[itemIndex].querySelector('.portfolio-img img').getAttribute('src');
-    lightboxImg.src = imgSrc;
-    lightboxText.innerHTML = portfolioItems[itemIndex].querySelector('h4').innerHTML;
-    lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalPortfolioItem;
-}
-
-function prevItem() {
-    if (itemIndex === 0) {
-        itemIndex = totalPortfolioItem - 1;
-    } else {
-        itemIndex--;
-    }
-    changeItem();
-}
-
-function nextItem() {
-    if (itemIndex === totalPortfolioItem - 1) {
-        itemIndex = 0;
-    } else {
-        itemIndex++;
-    }
-    changeItem();
-}
-
-// close lightbox
-
-lightbox.addEventListener('click', function(event){
-    if(event.target === lightboxClose || event.target === lightbox){
-        toggleLightbox();
-    }
-});
 
 // Aside Navbar
-
 const nav = document.querySelector('.nav'),
-    navList = nav.querySelectorAll('li'),
-    totalNavList = navList.length,
-    allSection = document.querySelectorAll('.section'),
-    totalSection = allSection.length;
+    allSection = document.querySelectorAll('.section');
 
-for (let i = 0; i < totalNavList; i++) {
-    const a = navList[i].querySelector('a');
-    a.addEventListener('click', function(){
-        // remove back section class
-        removeBackSectionClass();
+if(nav) {
+    const navList = nav.querySelectorAll('li'),
+        totalNavList = navList.length,
+        totalSection = allSection.length;
 
-        for (let j = 0; j < totalNavList; j++) {
-            if (navList[j].querySelector('a').classList.contains('active')) {
-                // add back section class
-                addBackSectionClass(j);
-            }
-            navList[j].querySelector('a').classList.remove('active');
-        }
-
-        this.classList.add('active');
-
-        showSection(this);
-
-        if (window.innerWidth < 1200) {
-            asideSectionTogglerBtn();
-        }
-
-    });
-}
-
-function addBackSectionClass(num) 
-{
-    allSection[num].classList.add('back-section');
-}
-
-function removeBackSectionClass() 
-{
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.remove('back-section');
-    }
-}
-
-function updateNav(element) 
-{
     for (let i = 0; i < totalNavList; i++) {
-        navList[i].querySelector('a').classList.remove('active');
-        const target = element.getAttribute('href').split('#')[1];
-        if (target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) {
-            navList[i].querySelector('a').classList.add('active');
+        const a = navList[i].querySelector('a');
+        a.addEventListener('click', function(){
+            // remove back section class
+            removeBackSectionClass();
+
+            for (let j = 0; j < totalNavList; j++) {
+                if (navList[j].querySelector('a').classList.contains('active')) {
+                    // add back section class
+                    addBackSectionClass(j);
+                }
+                navList[j].querySelector('a').classList.remove('active');
+            }
+
+            this.classList.add('active');
+
+            showSection(this);
+
+            if (window.innerWidth < 1200) {
+                asideSectionTogglerBtn();
+            }
+        });
+    }
+
+    function addBackSectionClass(num)
+    {
+        allSection[num].classList.add('back-section');
+    }
+
+    function removeBackSectionClass()
+    {
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove('back-section');
         }
     }
-}
 
-document.querySelector('.hire-me').addEventListener('click', function(){
-    const sectionIndex = this.getAttribute('data-section-index');
-    addBackSectionClass(sectionIndex);
-    showSection(this);
-    updateNav(this);
-    removeBackSectionClass();
-});
-
-function showSection(element) 
-{
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.remove('active');
+    function updateNav(element)
+    {
+        for (let i = 0; i < totalNavList; i++) {
+            navList[i].querySelector('a').classList.remove('active');
+            const target = element.getAttribute('href').split('#')[1];
+            if (target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) {
+                navList[i].querySelector('a').classList.add('active');
+            }
+        }
     }
 
-    const target = element.getAttribute('href').split('#')[1];
+    if(document.querySelector('.hire-me')) {
+        document.querySelector('.hire-me').addEventListener('click', function(){
+            const sectionIndex = this.getAttribute('data-section-index');
+            addBackSectionClass(sectionIndex);
+            showSection(this);
+            updateNav(this);
+            removeBackSectionClass();
+        });
+    }
 
-    document.querySelector('#'+target).classList.add('active');
+    function showSection(element)
+    {
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove('active');
+        }
+
+        const target = element.getAttribute('href').split('#')[1];
+        document.querySelector('#'+target).classList.add('active');
+    }
 }
 
+// Mobile Navigation Toggle
 const navTogglerBtn = document.querySelector('.nav-toggler'),
     aside = document.querySelector('.aside');
 
-navTogglerBtn.addEventListener('click', asideSectionTogglerBtn);
+if(navTogglerBtn && aside) {
+    navTogglerBtn.addEventListener('click', function() {
+        asideSectionTogglerBtn();
+    });
 
-function asideSectionTogglerBtn() 
-{
-    aside.classList.toggle('open');
-    navTogglerBtn.classList.toggle('open');
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.toggle('open');
+    function asideSectionTogglerBtn()
+    {
+        aside.classList.toggle('open');
+        navTogglerBtn.classList.toggle('open');
+
+        // Toggle open class for all sections only for layout purposes
+        for (let i = 0; i < allSection.length; i++) {
+            allSection[i].classList.toggle('open');
+        }
     }
 }
+
+// Initialize mobile navigation on page load
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.innerWidth < 1200) {
+        // Make sure the nav toggler is visible
+        if(navTogglerBtn) {
+            navTogglerBtn.style.display = 'flex';
+        }
+    }
+});
